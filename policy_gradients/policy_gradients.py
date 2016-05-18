54387cee7c334ce0dcd9fba8ea94f1760c32eb20
domluna <dluna132@gmail.com>
2016-05-18 03:05:54 -0400

    mostly tidy up, the seed is a bad one for our initialization so we have to let the iterations run for a bit before good progress is made

--- policy_gradients/policy_gradients.py
+++ policy_gradients/policy_gradients.py
@@ -47,12 +47,12 @@ from __future__ import print_function
 from __future__ import division
 
 from six.moves import range
+from gym.spaces import Box, Discrete
+from scipy.signal import lfilter
 
 import gym
-from gym.spaces import Box, Discrete
 import tensorflow as tf
 import numpy as np
-from scipy.signal import lfilter
 import argparse
 
 def flatten_space(space):
@@ -66,41 +66,30 @@ def flatten_space(space):
 def discount_cumsum(x, gamma):
     return lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
 
-def MLP(in_dim, out_dim, hidden_size, initializer,
-        hidden_nonlin=tf.tanh,
-        out_nonlin=tf.nn.softmax):
-    """Simple MultiLayer Perceptron"""
-    def _f(x):
-        W1 = tf.Variable(initializer([in_dim, hidden_size]))
-        W2 = tf.Variable(initializer([hidden_size, out_dim]))
-        b1 = tf.Variable(tf.zeros([hidden_size]))
-        b2 = tf.Variable(tf.zeros([out_dim]))
-        h1 = hidden_nonlin(tf.nn.xw_plus_b(x, W1, b1))
-        return out_nonlin(tf.nn.xw_plus_b(h1, W2, b2))
-    return _f
-
 
 class CategoricalPolicy(object):
-    def __init__(self, mlp, optimizer, session):
+    def __init__(self, in_dim, out_dim, hidden_dim, optimizer, session):
 
         # Placeholder Inputs
-        self._observations = tf.placeholder(tf.float32, name="observations")
+        self._observations = tf.placeholder(tf.float32, shape=[None, in_dim], name="observations")
         self._actions = tf.placeholder(tf.int32, name="actions")
         self._advantages = tf.placeholder(tf.float32, name="advantages")
 
         self._opt = optimizer
         self._sess = session
 
-        probs = mlp(self._observations)
+        h1 = tf.contrib.layers.fully_connected(self._observations, hidden_dim, activation_fn=tf.tanh)
+        probs = tf.contrib.layers.fully_connected(h1, out_dim, activation_fn=tf.nn.softmax)
 
         # NOTE: Doesn't currently work due to gather_nd gradient not being currently implemented
         # inds = tf.transpose(tf.pack([tf.range(tf.shape(probs)[0]), self._actions]))
         # log_lik = tf.log(tf.gather_nd(probs, inds))
 
         idxs_flattened = tf.range(0, tf.shape(probs)[0]) * tf.shape(probs)[1] + self._actions
-        log_lik = tf.log(tf.gather(tf.reshape(probs, [-1]), idxs_flattened) + 1e-8)
+        log_lik = -tf.log(tf.gather(tf.reshape(probs, [-1]), idxs_flattened) + 1e-8)
 
-        act_op = tf.argmax(probs, 1, name="act_op")
+
+        act_op = probs[0, :]
         loss_op = tf.reduce_mean(log_lik * self._advantages, name="loss_op")
         train_op = self._opt.minimize(loss_op, name="train_op")
 
@@ -111,7 +100,9 @@ class CategoricalPolicy(object):
     def act(self, observation):
         # expect observation to be shape(1, self.observation_space)
         a = self._sess.run(self._act_op, feed_dict={self._observations: observation})
-        return a[0] 
+        cs = np.cumsum(a)
+        idx = sum(cs < np.random.rand())
+        return idx
 
     def train(self, observations, actions, advantages):
         loss, _ = self._sess.run([self._loss_op, self._train_op], feed_dict={self._observations:observations, self._actions:actions, self._advantages:advantages})
@@ -138,7 +129,7 @@ class PolicyOptimizer(object):
 
         for _ in range(self.path_length):
             a = self.policy.act(ob.reshape(1, -1))
-            (next_ob, r, done, _) = self.env.step(a)
+            next_ob, r, done, _ = self.env.step(a)
             obs.append(ob)
             actions.append(a)
             rewards.append(r)
@@ -161,7 +152,8 @@ class PolicyOptimizer(object):
             a = r - b
 
             p["returns"] = r
-            p["advantages"] = (a - a.mean()) / (a.std() + 1e-8) # normalize
+            # p["advantages"] = (a - a.mean()) / (a.std() + 1e-8) # normalize
+            p["advantages"] = a
             p["baselines"] = b
 
         obs = np.concatenate([ p["observations"] for p in paths ])
@@ -194,7 +186,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_iter', default=100, type=int, help='number of iterations')
     parser.add_argument('--n_episode', default=100, type=int, help='number of episodes/iteration')
-    parser.add_argument('--learning_rate', default=1e-1, help='learning rate for Adam Optimizer')
+    parser.add_argument('--path_length', default=200, type=int, help='number of steps')
+    parser.add_argument('--learning_rate', default=0.01, help='learning rate for Adam Optimizer')
     parser.add_argument('--algorithm', default='Vanilla Policy Gradient', help='algorithm identifier')
     parser.add_argument('--outdir', default='vanilla-policy-gradient-CartPole-v0', type=str, help='output directory where results are saved (/tmp/ prefixed)')
     parser.add_argument('--upload', action='store_true', help='upload results via OpenAI Gym API')
@@ -210,19 +203,17 @@ if __name__ == '__main__':
 
     sess = tf.Session()
 
-    horizon = env.spec.timestep_limit
     in_dim = flatten_space(env.observation_space)
     out_dim = flatten_space(env.action_space)
-    hidden_size = 8
-
-    initializer = tf.contrib.layers.xavier_initializer()
-    mlp = MLP(in_dim, out_dim, hidden_size, initializer)
+    hidden_dim = 8
 
     opt = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
-    policy = CategoricalPolicy(mlp, opt, sess)
-    po = PolicyOptimizer(env, policy, 0, args.n_iter, args.n_episode, horizon)
+    policy = CategoricalPolicy(in_dim, out_dim, hidden_dim, opt, sess)
+    po = PolicyOptimizer(env, policy, 0, args.n_iter, args.n_episode, args.path_length)
 
     sess.run(tf.initialize_all_variables())
+
+    print(sess.run(tf.trainable_variables()))
 
     # train the policy optimizer
     po.train()
