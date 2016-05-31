694de29477e4f01e74d635ebe325459f3d8aec7e
Dominique Luna <dluna132@gmail.com>
2016-05-31 16:47:10 -0400

    remove deque import

--- q_learning/experience_replay.py
+++ q_learning/experience_replay.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from collections import deque
-
 import numpy as np
 
 class ExperienceReplay(object):
